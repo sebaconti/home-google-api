98a43c453dfaa413a48a5834e73756bf65ecfd00
sebaconti <sebaconti19@gmail.com>
2021-06-03 20:31:25 -0300

    1.0.1

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 
+const router = express.Router();
 const app = express();
 const server = require("http").Server(app);
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.send("Servidor abierto");
     console.log(req.body);
 });
 
-app.post("/api/google", (req, res) => {
+router.post("/api/google", (req, res) => {
     console.log(req.body);
 })
+
+app.use("/", router);
 
 console.log(process.env.PORT);
 server.listen(process.env.PORT);
